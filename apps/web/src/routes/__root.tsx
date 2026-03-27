20bc9076619f9120bb94d0183f90a5c1a22a3ef8
iRazz <hi@irazz.lol>
2026-03-27 02:53:41 +0200

    rel

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -25,7 +25,7 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "My App",
+        title: "irazz.lol",
       },
     ],
     links: [
