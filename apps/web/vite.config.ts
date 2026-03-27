01b5035a772ad085e3752db764f70bab10a7e71d
iRazz <hi@irazz.lol>
2026-03-27 10:42:10 +0200

    better cloudflare compat

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,9 +3,10 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact(), cloudflare({ viteEnvironment: { name: 'ssr' } }),],
   server: {
     port: 3001,
   },
