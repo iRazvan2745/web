de2ed264ae0aa78fa768e3716af1b3a75b155979
iRazz <hi@irazz.lol>
2026-04-30 10:30:59 +0300

    mobile warning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,16 @@
 	  import Footer from "$lib/components/sections/footer.svelte";
     import SecondSection from "$lib/components/sections/second.svelte"
     import ThirdSection from "$lib/components/sections/third.svelte"
+	import { m } from "$lib/paraglide/messages";
 </script>
 
 <main class="border-foreground">
     <div class="text-foreground">
-      <!-- <div class="sticky inset-0 top-14.25 flex items-center justify-center z-20 lg:hidden mx-auto">
+      <div class="sticky inset-0 top-14.25 flex items-center justify-center z-20 lg:hidden mx-auto">
         <p class="text-foreground bg-background text-lg font-mono border-b border-x px-1">
-          use a device with a wider screen pls
+          {m.desktop_recommended()}
         </p>
-      </div> -->
+      </div>
       <FirstSection />
       <MyWork />
       <SecondSection />
